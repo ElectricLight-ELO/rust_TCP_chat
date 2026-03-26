a869676e4101229f0920eddf051191000e16dc1e
ElectricLight-ELO <161233435+ElectricLight-ELO@users.noreply.github.com>
2026-03-26 11:10:09 +0300

    test2

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,14 @@
 
 fn main() {
-    let x = 5; 
-    let x = x + 1;      // x теперь 6
-    let x = x * 2; 
-    println!("The value of x is: {}", x); // Выведет "The value of x is: 12"
+
+
+    let num = 7;
+
+    if num > 5 {
+        println!("> 5");
+    } else if num == 5 {
+        println!("= 5");
+    } else {
+        println!("< 5");
+    }
 }
