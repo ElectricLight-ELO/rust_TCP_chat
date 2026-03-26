52bb6b61ebd2d2f67104172ae4583a301763caf8
ElectricLight-ELO <161233435+ElectricLight-ELO@users.noreply.github.com>
2026-03-26 11:08:20 +0300

    test1

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,7 @@
+
 fn main() {
-    println!("Hello, world!");
+    let x = 5; 
+    let x = x + 1;      // x теперь 6
+    let x = x * 2; 
+    println!("The value of x is: {}", x); // Выведет "The value of x is: 12"
 }
